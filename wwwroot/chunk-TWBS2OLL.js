4c3298c9b5be8845bed9ce3c1c3d164af1246cc5
Rajanish Maury <rajanish.buz@gmail.com>
2026-06-13 13:30:57 +0530

    Fix: remove leftover locating ref (build was failing -> empty wwwroot -> 404)

---
+++ wwwroot/chunk-TWBS2OLL.js
@@ -0,0 +1,1 @@
+import{d as r}from"./chunk-VR7JJUMP.js";import{f as e}from"./chunk-EQDQRRRY.js";var o=class extends r{canOpenUrl(n){return e(this,null,function*(){return{value:!0}})}openUrl(n){return e(this,null,function*(){return window.open(n.url,"_blank"),{completed:!0}})}};export{o as AppLauncherWeb};
